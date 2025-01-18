1ece17199096257fde6c63f4637c61d409ad5a27
Administrator <Administrator@pc54>
2025-01-18 18:11:26 +0900

    2025.01.18/문제1, 문제2, 문제3 풀이

--- homework1/src/h_pakage1/H_class.java
+++ homework1/src/h_pakage1/H_class.java
@@ -4,6 +4,23 @@ public class H_class {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int cigar = 2;
+		int smokeYear = 20;
+		int dailyCigarNo = 3;
+		int year = 365;
+		
+		int oneLifeLost = cigar * dailyCigarNo;
+		int smokedays = smokeYear * year;
+		
+		int question1 = smokedays * oneLifeLost;
+		int question2 = question1 / 60;
+		int question3 = question2 / 24;
+
+		System.out.println(question1);
+		System.out.println(question2);
+		System.out.println(question3);
+		
 
 	}
 
